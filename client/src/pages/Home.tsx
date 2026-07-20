9e3dba4ccde7c98cd412a4f4194cae451004f543
Gustavo Ribeiro <guprs.tech@gmail.com>
2026-07-20 16:34:17 +0200

    feat: frontend scaffold with vite, react, typescript, tailwind, router and axios

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,11 @@
+function Home() {
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-blue-100">
+            <h1 className="text-4xl font-bold text-blue-600">
+            High Five! 🖐️
+            </h1>
+        </div>
+    )
+}
+
+export default Home
