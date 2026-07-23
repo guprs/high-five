60570c4e8df28e08a9decc7ed16f72fbfc47588d
Gustavo Ribeiro <guprs.tech@gmail.com>
2026-07-23 12:46:19 +0200

    feat: child and task crud endpoints

---
+++ server/src/routes/task.routes.ts
@@ -0,0 +1,21 @@
+import { Router } from 'express';
+import {
+    createTask,
+    getTasks,
+    getTaskById,
+    updateTask,
+    deleteTask,
+} from '../controllers/task.controller';
+import { authenticate } from '../middleware/auth.middleware';
+
+const router = Router();
+
+router.use(authenticate);
+
+router.post('/', createTask);
+router.get('/', getTasks);
+router.get('/:id', getTaskById);
+router.patch('/:id', updateTask);
+router.delete('/:id', deleteTask);
+
+export default router;
